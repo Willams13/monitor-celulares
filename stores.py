95407ed7eafae5d59be7fd1ba023838e45b0b832
Willams13 <63917844+Willams13@users.noreply.github.com>
2026-07-29 11:47:36 -0300

    Update stores.py

--- stores.py
+++ stores.py
@@ -1,12 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def buscar_precos():
 
-    print("TESTE STORES FUNCIONANDO")
-
-    return [
-        {
-            "nome": "Samsung Galaxy S25 FE TESTE",
-            "preco": 2500,
-            "loja": "Teste",
-            "link": "https://google.com"
-        }
+    links = [
+        "https://www.tudocelular.com/Samsung/fichas-tecnicas/n10155/Samsung-Galaxy-S25-FE.htm",
+        "https://www.tudocelular.com/Poco/fichas-tecnicas/n10349/Poco-X8-Pro-Max.html"
     ]
+
+    for link in links:
+
+        resposta = requests.get(
+            link,
+            headers={
+                "User-Agent": "Mozilla/5.0"
+            },
+            timeout=10
+        )
+
+        soup = BeautifulSoup(
+            resposta.text,
+            "html.parser"
+        )
+
+        texto = soup.get_text(
+            " ",
+            strip=True
+        )
+
+        print("LINK:", link)
+        print(texto[:1000])
+        print("------------------------")
+
+
+    return []
