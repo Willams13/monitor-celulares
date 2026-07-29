9ac229b2bb43f476aea9ebea7525f1168c91adf6
Willams13 <63917844+Willams13@users.noreply.github.com>
2026-07-29 11:07:37 -0300

    Update stores.py

--- stores.py
+++ stores.py
@@ -1,25 +1,10 @@
-from duckduckgo_search import DDGS
-
-
 def buscar_precos():
 
-    resultados = []
-
-    with DDGS() as ddgs:
-
-        pesquisa = ddgs.text(
-            "Samsung Galaxy S25 FE preço Brasil",
-            max_results=5
-        )
-
-        for item in pesquisa:
-
-            resultados.append({
-                "nome": item["title"],
-                "preco": 9999,
-                "loja": "Teste",
-                "link": item["href"]
-            })
-
-
-    return resultados
+    return [
+        {
+            "nome": "Teste S25 FE",
+            "preco": 2699,
+            "loja": "Teste",
+            "link": "https://google.com"
+        }
+    ]
