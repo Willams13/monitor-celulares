6a26ad4a478f883425a9d5ced8fce0df66e77b13
Willams13 <63917844+Willams13@users.noreply.github.com>
2026-07-29 08:57:18 -0300

    Create stores.py

---
+++ stores.py
@@ -0,0 +1,18 @@
+def buscar_precos():
+
+    produtos = [
+        {
+            "nome": "Samsung Galaxy S25 FE",
+            "preco": 2899,
+            "loja": "Loja teste",
+            "link": "https://exemplo.com"
+        },
+        {
+            "nome": "POCO X8 Pro Max",
+            "preco": 2699,
+            "loja": "Loja teste",
+            "link": "https://exemplo.com"
+        }
+    ]
+
+    return produtos
